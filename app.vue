9bb60613420d914b83320b0211eaf577e36f7a06
Sacha STAFYNIAK <sacha@digisquad.io>
2023-11-04 13:39:42 +0100

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const meta = await useComponentMeta('UButton')
+</script>
+
+<template>
+  <div>
+    <UButton>Button</UButton>
+    <pre>{{ meta.meta }}</pre>
+  </div>
+</template>
